26a0ccc37c469fa7eec7e7d33053f2652b2452d7
suuuuu017 <suuuuu017@ShiHes-MacBook-Pro.local>
2021-05-25 12:03:04 +0800

    finish project 1

--- projects/project1/p1.cpp
+++ projects/project1/p1.cpp
@@ -8,19 +8,63 @@ void print_prompt() {
 }
 
 bool armstrongNum(int num){
-
+    int i = 0;
+    while(i < num){
+        if((i ^ num % num) == i){
+            i = i + 1;
+        }
+        else{
+            return false;
+        }
+    }
+    return true;
 }
 
 bool cycloneJet(int num) {
-
+    int j = num;
+    int rev = 0;
+    while(j > 0){
+        rev = rev * 10 + j % 10;
+        j = j / 10;
+    }
+    if(rev == num){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool blackPremium(int num){
-
+    int x = 0;
+    x = (sqrt(24 * num + 1) + 1) / 6.0;
+    if(isdigit(x)){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 bool auspiciousNum(int num){
-
+    int sum = 0;
+    for (int i=2; i <= sqrt(num); i++){
+        if (num % i == 0){
+            if (num / i == i){
+                sum = sum + i;
+            }
+            else{
+                sum = sum + i + num / i;
+            }
+        }
+    }
+    sum = sum + 1;
+    if(sum > num){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 int main() {
@@ -38,6 +82,18 @@ int main() {
         if((1 <= op) && (op <= 4)){
             opCheck = true;
         }
+    }
+    if(op == 1){
+        std::cout << armstrongNum(integer);
+    }
+    if(op == 2){
+        std::cout << cycloneJet(integer);
+    }
+    if(op == 3){
+        std::cout << blackPremium(integer);
+    }
+    if(op == 4){
+        std::cout << auspiciousNum(integer);
     }
     return 0;
 }
