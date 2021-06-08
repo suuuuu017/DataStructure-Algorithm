d0c66d1db5f119346eb45ee92527961c76d740fc
suuuuu017 <suuuuu0177@outlook.com>
2021-06-09 01:29:46 +0800

    fix the bug with odd negative number

--- labs/lab3_starter_files/ex1.cpp
+++ labs/lab3_starter_files/ex1.cpp
@@ -12,7 +12,7 @@ int fold (int count, int arr[], int (*fn) (int, int), int initial) {
         return initial;
     }
     else{
-        return fn(array[count - 1], fold(count - 1, arr, fn, initial));
+        return fn(arr[count - 1], fold(count - 1, arr, fn, initial));
     }
 }
 
@@ -27,7 +27,7 @@ int fn_add (int a, int b) {
 int fn_count_odd (int a, int n) {
     // TODO: implement this function such that fold(n,arr,fn_count_odd, 0)
     // returns the numebr of odd numbers in the array
-    if((a % 2) == 1){
+    if(a % 2){
         return n + 1;
     }
     else{
