a1274e8f7d955cb55232f407ff93f43616a3a030
suuuuu017 <suuuuu0177@outlook.com>
2021-06-08 20:34:15 +0800

    finsih ex1 ex2 with problem in ex2

--- labs/lab3_starter_files/ex1.cpp
+++ labs/lab3_starter_files/ex1.cpp
@@ -8,18 +8,30 @@ using namespace std;
 int fold (int count, int arr[], int (*fn) (int, int), int initial) {
     // EFFECTS: returns the result of the fold function
     // TODO: implement this function recursively
+    if(count == 0){
+        return initial;
+    }
+    else{
+        fn(array[count - 1], fold(count - 1, arr, fn, initial));
+    }
 }
 
 
 int fn_add (int a, int b) {
     // TODO: implement this function such that fold(count, arr, fn_add, 0)
     // returns the sum of all the elements in arr
-    
+    return (a + b);
 }
 
 
 int fn_count_odd (int a, int n) {
     // TODO: implement this function such that fold(n,arr,fn_count_odd, 0)
     // returns the numebr of odd numbers in the array
+    if((a % 2) == 1){
+        return n = n + 1;
+    }
+    else{
+        return n;
+    }
 }
 
--- labs/lab3_starter_files/ex2.cpp
+++ labs/lab3_starter_files/ex2.cpp
@@ -8,13 +8,31 @@
 using namespace std;
 
 const int MAXSIZE = 10;
+const int winning = 280;
+
+bool canWin_helper(bool visit[], int count, int card[], int position){
+    visit[position] = true;
+    if(card[position] == winning){
+        return true;
+    }
+//    else if((position + card[position] < count) && (! visit[position + card[position]])){
+//        canWin_helper(visit, count, card, position + card[position]);
+//    }
+//    else if((position - card[position] >= 0) && (! visit[position - card[position]])){
+//        canWin_helper(visit, count, card, position - card[position]);
+//    }
+    return false;
+}
 
 bool canWin(int count, int arr[], int position) {
     // EFFECTS: return whether the player can win given the start position
     // and the card sequence
     
     // TODO: implement this function
+    bool visit[MAXSIZE] = {false};
+    return canWin_helper(visit, count, arr, position);
 }
+
 
 int main() {
     int count;
