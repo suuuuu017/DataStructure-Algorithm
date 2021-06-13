1a9905f7e3b8f01d7e15cc4142ea1d051ab731d8
suuuuu017 <suuuuu0177@outlook.com>
2021-06-14 00:41:01 +0800

    finish lab1 first version

--- labs/lab4_starter_files/ex1.cpp
+++ labs/lab4_starter_files/ex1.cpp
@@ -5,13 +5,38 @@ using namespace std;
 // Classes for exception types. They are just dummy types for you to use when
 // handling expections.
 // TODO: Define needed classes for exception.
+struct costumeNeeded{
+    int costume;
+};
 
+struct gunNeeded{
+    int gun;
+};
+
+struct bulletNeeded{
+    int bullet;
+};
+
+struct closeO{
+};
+
+struct closeY{
+};
+
+costumeNeeded costumeN;
+gunNeeded gunN;
+bulletNeeded bulletN;
 
 // EFFECTS: Check whether there are enough costumes in Omori's Costume Store. If not, throw
 //          a exception.
 void buy_costumes(int num_scenes, int costume_remain)
 {
     // TODO:
+    int cmp = costume_remain - num_scenes;
+    if(cmp < 0){
+        costumeN.costume = cmp * (-1);
+        throw costumeN;
+    }
 }
 
 // EFFECTS: Check whether there are enough guns in Yamazaki's Model Shop. If not, throw
@@ -19,6 +44,11 @@ void buy_costumes(int num_scenes, int costume_remain)
 void buy_guns(int num_scenes, int guns_remain)
 {
     // TODO:
+    int cmp = guns_remain - 2 * num_scenes;
+    if(cmp < 0){
+        gunN.gun = cmp * (-1);
+        throw gunN;
+    }
 }
 
 // EFFECTS: Check whether there are enough bullets in Yamazaki's Model Shop. If not, throw
@@ -26,12 +56,24 @@ void buy_guns(int num_scenes, int guns_remain)
 void buy_bullets(int num_scenes, int bullets_remain)
 {
     // TODO:
+    int cmp = bullets_remain - 100 * num_scenes;
+    if(cmp < 0){
+        bulletN.bullet = cmp * (-1);
+        throw bulletN;
+    }
 }
 // EFFECTS: Check if Omori's Costume Store is open. If it is open, then go on to buy flour 
 //          and eggs. If it is closed, throw a exception.
 void visit_Omori(bool Omori_status, int num_scenes, int costume_remain)
 {
     // TODO:
+    if(!Omori_status){
+        closeO close;
+        throw close;
+    }
+    else{
+        buy_costumes(num_scenes, costume_remain);
+    }
 }
 
 // EFFECTS: Check if Yamazaki's Model Shop is open. If it is open, then go on to buy guns.
@@ -39,6 +81,14 @@ void visit_Omori(bool Omori_status, int num_scenes, int costume_remain)
 void visit_Yamazaki(bool Yamazaki_status, int num_scenes, int guns_remain, int bullets_remain)
 {
     // TODO:
+    if(!Yamazaki_status){
+        closeY close;
+        throw close;
+    }
+    else{
+        buy_guns(num_scenes, guns_remain);
+        buy_bullets(num_scenes, bullets_remain);
+    }
 }
 
 
@@ -54,7 +104,17 @@ int main(int argc, char* argv[])
     
     //TODO: 
     // Read all the above parameters from the program arguments
-
+    if(argc == 7){
+        num_scenes = atoi(argv[1]);
+        Omori_status = atoi(argv[2]);
+        Yamazaki_status = atoi(argv[3]);
+        costume_remain = atoi(argv[4]);
+        guns_remain = atoi(argv[5]);
+        bullets_remain = atoi(argv[6]);
+    }
+    else{
+        return 1;
+    }
     
     // If any expception happens, flag will be changed to 0.
     bool flag = 1;
@@ -65,38 +125,38 @@ int main(int argc, char* argv[])
     try 
     {
         // TODO: 
-
+        visit_Omori(Omori_status, num_scenes, costume_remain);
         cout << "We've bought enough costumes! I can't wait before Miss Asahina wears them! Then we visit Yamazaki's Model Shop..." << endl;
         try
         {
             // TODO: 
-
+            visit_Yamazaki(Yamazaki_status, num_scenes, guns_remain, bullets_remain);
             cout << "We've also bought enough guns and bullets! We can start the movies now! But Miss Asahina's nightmare is coming." << endl;
         }
-        catch( /* TODO: */ )
+        catch( closeY )
         {
             cout << "Yamazaki's Model Shop is closed!" << endl;
             flag = 0;
         }
-        catch( /* TODO: */ )
+        catch(struct gunNeeded)
         {
-            cout << "Guns in Yamazaki's Model Shop are not enough. We still need " << /* TODO: */ << " more." << endl;
+            cout << "Guns in Yamazaki's Model Shop are not enough. We still need " << gunN.gun << " more." << endl;
             flag = 0;
         }
-        catch( /* TODO: */ )
+        catch(struct bulletNeeded )
         {
-            cout << "Bullets in Yamazaki's Model Shop are not enough. We still need " << /* TODO: */ << " more." << endl;
+            cout << "Bullets in Yamazaki's Model Shop are not enough. We still need " << bulletN.bullet << " more." << endl;
             flag = 0;
         }  
     }
-    catch( /* TODO: */ )
+    catch( closeO )
     {
         cout << "Omori's Costume Store is closed!" << endl;
         flag = 0;
     }
-    catch( /* TODO: */ )
+    catch(struct costumeNeeded )
     {
-        cout << "Costumes in Omori's Costume Store are not enough. We still need " <<  /* TODO: */  << " more." << endl;
+        cout << "Costumes in Omori's Costume Store are not enough. We still need " <<  costumeN.costume  << " more." << endl;
         flag = 0;
     }
 
