11be536d2db521697f806944e4a9d7aa04b19cfe
suuuuu017 <suuuuu0177@outlook.com>
2021-07-10 01:09:56 +0800

    finish hand.cpp

---
+++ projects/project3_starter/hand.cpp
@@ -0,0 +1,39 @@
+#include "hand.h"
+
+Hand::Hand() {
+    curValue.count = 0;
+    curValue.soft = false;
+}
+
+void Hand::discardAll() {
+    curValue.count = 0;
+    curValue.soft = false;
+}
+
+void Hand::addCard(Card c) {
+    if(c.spot == ACE && curValue.soft){
+        curValue.count = curValue.count + 1;
+    }
+    else if(c.spot == ACE && (!curValue.soft)){
+        curValue.count = curValue.count + 11;
+        curValue.soft = true;
+    }
+    else if(c.spot == JACK || c.spot == QUEEN || c.spot == KING){
+        curValue.count = curValue.count + 10;
+    }
+    else{
+        curValue.count = curValue.count + c.spot + 2;
+    }
+
+    if(curValue.count > 21 && curValue.soft){
+        curValue.count = curValue.count - 10;
+        curValue.soft = false;
+    }
+}
+
+HandValue Hand::handValue() const {
+    return curValue;
+}
+
+
+
