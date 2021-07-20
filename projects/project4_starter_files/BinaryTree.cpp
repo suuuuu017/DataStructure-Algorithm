69ee56532ae3192c161f12a1c55fd36deb85c033
suuuuu017 <suuuuu0177@outlook.com>
2021-07-20 19:55:36 +0800

    add comments for helper functions

--- projects/project4_starter_files/BinaryTree.cpp
+++ projects/project4_starter_files/BinaryTree.cpp
@@ -50,6 +50,8 @@ BinaryTree::BinaryTree(const int &rootValue):root(new Node(rootValue)){
 BinaryTree::BinaryTree(Node *node):root(node){
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: returns a root node for the tree
 Node* copyNodeHelper(const Node* source){
     if(!source){
         return nullptr;
@@ -68,6 +70,9 @@ BinaryTree::BinaryTree(std::vector<nodeValue> &source) {
     this->root = createFromVariant(source, 0);
 }
 
+//REQUIRES: a input source tree root node
+//MODIFIES: the input tree
+//EFFECTS: delete the tree
 void deleteHelper(Node * node){
     if(node){
         deleteHelper(node->getLeft());
@@ -84,6 +89,8 @@ bool BinaryTree::empty() const {
     return !root;
 }
 
+//REQUIRES: a input source tree root node and the target key
+//EFFECTS: return nullptr if not found or the node with the target key if found
 Node* findHelper(const int &key, Node * node){
     if(!node){return nullptr;}
     if(node->getVal() == key){return node;}
@@ -103,6 +110,8 @@ Node *BinaryTree::find(const int &key) const {
     return findHelper(key, this->root);
 }
 
+//REQUIRES: a input source tree root node and the target key
+//EFFECTS: return "" if not found or path to the node with the val
 bool findPathHelper(const int &value, Node * node, std::string & output){
     if(!node){return false;}
     if(node->getVal() == value){return true;}
@@ -121,6 +130,8 @@ std::string BinaryTree::findPath(const int &value) const {
     return output;
 }
 
+//REQUIRES: a input source tree root node and a path
+//EFFECTS: return nullptr if not found or the node reachable from the path
 Node * visitThroughHelper(string &path, Node *node){
     if(path.empty() && node){return node;}
     else if(node && (!path.empty())){
@@ -141,6 +152,8 @@ Node *BinaryTree::visitThroughPath(const string &path) const {
     return visitThroughHelper(tmpPath, this->root);
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: return sum of the nodes in a tree
 int sumHelper(Node * node){
     if(!node){
         return 0;
@@ -152,6 +165,8 @@ int BinaryTree::sum() const {
     return sumHelper(this->root);
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: return height of a tree
 int heightHelper(Node * node){
     if(!node){return 0;}
     int lDepth = heightHelper(node->getLeft());
@@ -168,6 +183,8 @@ int BinaryTree::height() const {
     return heightHelper(this->root);
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: return the preorder traversal results
 void preOrderHelper(Node * node){
     if(!node){
         return;
@@ -182,6 +199,8 @@ void BinaryTree::preOrder() const {
     cout << endl;
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: return the inorder traversal results
 void inOrderHelper(Node * node){
     if(!node){
         return;
@@ -196,6 +215,8 @@ void BinaryTree::inOrder() const {
     cout << endl;
 }
 
+//REQUIRES: a input source tree root node
+//EFFECTS: return the postorder traversal results
 void postOrderHelper(Node * node){
     if(!node){
         return;
@@ -210,6 +231,8 @@ void BinaryTree::postOrder() const {
     cout << endl;
 }
 
+//REQUIRES: a input source tree root node and the sum bar
+//EFFECTS: return true if the sum along any path is larger than the bar and false otherwise
 bool pathSumGreaterHelper(int sum, Node * node){
     if(!node){
         return true;
@@ -226,6 +249,8 @@ bool BinaryTree::allPathSumGreater(const int &sum) const {
     return pathSumGreaterHelper(tmpSum, this->root);
 }
 
+//REQUIRES: a input source tree root node and the tree to test again
+//EFFECTS: return true if the tree is covered by the tested-again tree and false otherwise
 bool coverHelper(Node * thisNode, Node * thatNode){
     if(!thisNode && !thatNode){return true;}
     else if(!thisNode && thatNode){return true;}
@@ -239,6 +264,8 @@ bool BinaryTree::operator<(const BinaryTree &tree) const {
     return coverHelper(this->root, tree.root);
 }
 
+//REQUIRES: a input source tree root node and the tree to test again
+//EFFECTS: return true if the tree is contained by the tested-again tree and false otherwise
 bool containHelper(Node * thisnode, Node * thatnode){
     if(!thatnode){
         return false;
--- projects/project4_starter_files/test.cpp
+++ projects/project4_starter_files/test.cpp
@@ -25,13 +25,13 @@ void nodePrint(Node *x) {
 
 int main() {
 //    vector<nodeValue> source = {4, 2, 5, 7, 3, "$", "@", "hey", "xm", 8, 9};
-//    vector<nodeValue> source = {1, 2, 3, 4, 5, "!", 6, 7, 8, 9, "?", 10, 11};
-//    BinaryTree tree(source);
+    vector<nodeValue> source = {1, 2, 3, 4, 5, "!", 6, 7, 8, 9, "?", 10, 11};
+    BinaryTree tree(source);
 //    BinaryTree waste(tree);
 //    treeCheck(tree);
 //
-//    Node *x = tree.find(9);
-//    cout << x->getVal() << endl;
+//    Node *x = tree.findPath(12);
+    cout << tree.findPath(12) << endl;
 //    x->setLeft(100);
 //    cout <<"path to 100 " <<  tree.findPath(100) << endl;
 //
@@ -54,32 +54,32 @@ int main() {
 //    x = tree.find(101);
 //    x->setLeft(102);
 //
-//    cout <<"path to 102 " <<  tree.findPath(102) << endl;
-    vector<nodeValue> coveredVec = {4, 2, 5};
-//    vector<nodeValue> coveredVec = {3, 6, 7};
-    vector<nodeValue> uncoveredVec1 = {4, 3};
-    vector<nodeValue> uncoveredVec2 = {5};
-//    vector<nodeValue> uncoveredVec = {3, "!", 7};
-    vector<nodeValue> coveredTargetVec = {4, 2, 5, "", 3};
-    BinaryTree covered(coveredVec);
-    BinaryTree uncovered1(uncoveredVec1);
-    BinaryTree uncovered2(uncoveredVec2);
-    BinaryTree coveredTarget(coveredTargetVec);
-    cout << "cover " << (covered < coveredTarget) << endl;
-    cout << "uncover1 " << (uncovered1 < coveredTarget) << endl;
-    cout << "uncover2 " << (uncovered2 < coveredTarget) << endl;
-
-    vector<nodeValue> containedVec = {3, 6, 7};
-    vector<nodeValue> notContainedVec1 = {4, 2, 5, "", 3, 7, 8};
-    vector<nodeValue> notContainedVec2 = {3, "!", 7};
-    vector<nodeValue> containedTargetVec = {4, 2, 5, "", 3, "", 8, "", "", 6, 7};
-    BinaryTree contained(containedVec);
-    BinaryTree notContained1(notContainedVec1);
-    BinaryTree notContained2(notContainedVec2);
-    BinaryTree containedTarget(containedTargetVec);
-    cout << "contained " << (contained << containedTarget) << endl;
-    cout << "notcontained1 " << (notContained1 << containedTarget) << endl;
-    cout << "contained2 " << (notContained2 << containedTarget) << endl;
+////    cout <<"path to 102 " <<  tree.findPath(102) << endl;
+//    vector<nodeValue> coveredVec = {4, 2, 5};
+////    vector<nodeValue> coveredVec = {3, 6, 7};
+//    vector<nodeValue> uncoveredVec1 = {4, 3};
+//    vector<nodeValue> uncoveredVec2 = {5};
+////    vector<nodeValue> uncoveredVec = {3, "!", 7};
+//    vector<nodeValue> coveredTargetVec = {4, 2, 5, "", 3};
+//    BinaryTree covered(coveredVec);
+//    BinaryTree uncovered1(uncoveredVec1);
+//    BinaryTree uncovered2(uncoveredVec2);
+//    BinaryTree coveredTarget(coveredTargetVec);
+//    cout << "cover " << (covered < coveredTarget) << endl;
+//    cout << "uncover1 " << (uncovered1 < coveredTarget) << endl;
+//    cout << "uncover2 " << (uncovered2 < coveredTarget) << endl;
+//
+//    vector<nodeValue> containedVec = {3, 6, 7};
+//    vector<nodeValue> notContainedVec1 = {4, 2, 5, "", 3, 7, 8};
+//    vector<nodeValue> notContainedVec2 = {3, "!", 7};
+//    vector<nodeValue> containedTargetVec = {4, 2, 5, "", 3, "", 8, "", "", 6, 7};
+//    BinaryTree contained(containedVec);
+//    BinaryTree notContained1(notContainedVec1);
+//    BinaryTree notContained2(notContainedVec2);
+//    BinaryTree containedTarget(containedTargetVec);
+//    cout << "contained " << (contained << containedTarget) << endl;
+//    cout << "notcontained1 " << (notContained1 << containedTarget) << endl;
+//    cout << "contained2 " << (notContained2 << containedTarget) << endl;
 //
 //    vector<nodeValue> sumGreaterVec = {4, 1, 5, 3, 6};
 //    BinaryTree sumGreater(sumGreaterVec);
