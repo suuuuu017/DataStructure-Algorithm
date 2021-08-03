c6523e82501a754ea4f54049151a540235cef72e
suuuuu017 <suuuuu0177@outlook.com>
2021-08-03 19:28:51 +0800

    finish first version

--- projects/Project-5-Related-Files/sam.cpp
+++ projects/Project-5-Related-Files/sam.cpp
@@ -53,6 +53,37 @@ void pushiInstr(Instr& it, Dlist<int> &stack){
     stack.insertFront(push);
 }
 
+void printMemory(int memoryArray[], int memoryLength){
+//    std::cout << "memory is ";
+    for(int i = 0; i < memoryLength; i++){
+        std::cout << memoryArray[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+void printStack(Dlist<int> &stack){
+//    std::cout << "resulting stack is " << stack << std::endl;
+    Dlist<int> tmpStack(stack);
+    while(!tmpStack.isEmpty()){
+        int * out = tmpStack.removeBack();
+        std::cout << *out << " ";
+    }
+    std::cout << std::endl;
+}
+
+void printQueue(Dlist<Instr> &queue){
+//    std::cout << "resulting queue is " << queue << std::endl;
+    std::cout << queue << std::endl;
+
+}
+
+void print(Instr& it, Dlist<int> &stack, Dlist<Instr> &queue, int memoryArray[], int memoryLength){
+    std::cout << it << std::endl;
+    printStack(stack);
+    printQueue(queue);
+    printMemory(memoryArray, memoryLength);
+}
+
 int main(int argc, char *argv[])
 {
     //TODO:write your code here.
@@ -71,17 +102,17 @@ int main(int argc, char *argv[])
     int queueNum = 0;
 
     while(getline(std::cin, line)) {
-        std::cout << line << std::endl;
+//        std::cout << line << std::endl;
         std::istringstream ss(line);
         ss >> stackNum >> queueNum;
         break;
     }
-    std::cout << stackNum << " " << queueNum << std::endl;
+//    std::cout << stackNum << " " << queueNum << std::endl;
 
     Dlist<int> stack;
 
     while(getline(std::cin, line)) {
-        std::cout << line << std::endl;
+//        std::cout << line << std::endl;
         std::istringstream ss(line);
         while(ss.peek() != EOF){
             int tmpVal = 0;
@@ -91,7 +122,7 @@ int main(int argc, char *argv[])
         }
         break;
     }
-    std::cout << "stack is " << stack << std::endl;
+//    std::cout << "stack is " << stack << std::endl;
 
     Dlist<Instr> queue;
 
@@ -101,11 +132,11 @@ int main(int argc, char *argv[])
         ss >> *instr;
         queue.insertBack(instr);
     }
-    std::cout << "queue is " << queue << std::endl;
+//    std::cout << "queue is " << queue << std::endl;
 
     Dlist<int> memory;
 
-    std::cout << line << std::endl;
+//    std::cout << line << std::endl;
     std::istringstream ss(line);
     int memoryLength = 0;
     while(ss.peek() != EOF){
@@ -115,57 +146,82 @@ int main(int argc, char *argv[])
         memory.insertBack(ip);
         memoryLength = memoryLength + 1;
     }
-    std::cout << "memory Length is " << memoryLength << std::endl;
+//    std::cout << "memory Length is " << memoryLength << std::endl;
 
     int memoryArray[memoryLength];
     for(int i = 0; i < memoryLength; i++){
         memoryArray[i] = *memory.removeFront();
     }
 
-    std::cout << "memory is ";
-    for(int i = 0; i < memoryLength; i++){
-        std::cout << memoryArray[i] << " ";
-    }
-    std::cout << std::endl;
+//    std::cout << "memory is ";
+//    for(int i = 0; i < memoryLength; i++){
+//        std::cout << memoryArray[i] << " ";
+//    }
+//    std::cout << std::endl;
 
     while(!queue.isEmpty()){
         Instr * currentIntr = queue.removeFront();
         if(currentIntr->name == InstrName::HALT){
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
             break;
         }
         else if(currentIntr->name == InstrName::ADD){
             addInstr(stack);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::NOR){
             norInstr(stack);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::IFZ){
             ifzInstr(*currentIntr, stack, queue);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::LOAD){
             loadInstr(stack, memoryArray);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::STORE){
             storeInstr(stack, memoryArray);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::POP){
             popInstr(stack);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::PUSHI){
             pushiInstr(*currentIntr, stack);
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
         }
         else if(currentIntr->name == InstrName::NOOP){
+            if(!slient){
+                print(*currentIntr, stack, queue, memoryArray, memoryLength);
+            }
             continue;
         }
     }
-
-    std::cout << "resulting stack is " << stack << std::endl;
-    std::cout << "resulting queue is " << queue << std::endl;
-    std::cout << "memory is ";
-    for(int i = 0; i < memoryLength; i++){
-        std::cout << memoryArray[i] << " ";
+    if(slient){
+        printStack(stack);
+        printMemory(memoryArray, memoryLength);
     }
-    std::cout << std::endl;
+
+
 
     return 0;
 }
